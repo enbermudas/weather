355fdf65f56d23701e9c0017f39fee75969b6b88
Enrique Bermúdez <enbermudez96@gmail.com>
2020-06-08 16:22:20 -0300

    feat(config): add API_KEY to dotenv config

--- src/config/env.js
+++ src/config/env.js
@@ -2,10 +2,11 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const { NODE_ENV, PORT, HOST } = process.env;
+const { NODE_ENV, PORT, HOST, API_KEY } = process.env;
 
 module.exports = {
   NODE_ENV: NODE_ENV || 'dev',
   HOST: HOST || '0.0.0.0',
-  PORT: PORT || 8000
+  PORT: PORT || 8000,
+  API_KEY: API_KEY || ''
 };
