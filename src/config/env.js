e9fa35d7e6a41b7b456b9dbcee8ed52e9a1824f5
Enrique Bermúdez <enbermudez96@gmail.com>
2020-06-08 14:29:45 -0300

    feat(config): add port and host to dotenv config

--- src/config/env.js
+++ src/config/env.js
@@ -2,8 +2,10 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const { NODE_ENV } = process.env;
+const { NODE_ENV, PORT, HOST } = process.env;
 
 module.exports = {
-  NODE_ENV: NODE_ENV || 'dev'
+  NODE_ENV: NODE_ENV || 'dev',
+  HOST: HOST || '0.0.0.0',
+  PORT: PORT || 8000
 };
