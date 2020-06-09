494871502b564a283ab78457b1a8b8aca6c29cb8
Enrique Bermúdez <enbermudez96@gmail.com>
2020-06-09 14:29:58 -0300

    test: update weather tests to include forecast

--- tests/weather.test.js
+++ tests/weather.test.js
@@ -25,7 +25,10 @@ describe('(CONFIG) WEATHER BIT', () => {
         .post('/api/v1/weather/current')
         .end((err, res) => {
           expect(res.body).to.have.property('data');
+          expect(res.body.data).to.be.an('array');
+          expect(res.body.data.length).to.eql(1);
           expect(res.body).to.have.property('count');
+          expect(res).to.have.status(200);
           done();
         });
     });
@@ -38,7 +41,50 @@ describe('(CONFIG) WEATHER BIT', () => {
         })
         .end((err, res) => {
           expect(res.body).to.have.property('data');
+          expect(res.body.data).to.be.an('array');
+          expect(res.body.data.length).to.eql(1);
           expect(res.body).to.have.property('count');
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe('FORECAST', () => {
+    it('should fail if non-existent city name is provided', (done) => {
+      request(server)
+        .post('/api/v1/weather/forecast')
+        .send({
+          name: 'non-existent-city-name'
+        })
+        .end((err, res) => {
+          expect(res.body).to.have.property('message');
+          expect(res).to.have.status(500);
+          done();
+        });
+    });
+
+    it('should succeed if no city name is provided (default fetch with ipapi)', (done) => {
+      request(server)
+        .post('/api/v1/weather/forecast')
+        .end((err, res) => {
+          expect(res.body).to.have.property('data');
+          expect(res.body.data).to.be.an('array');
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+
+    it('should succeed and return the forecast', (done) => {
+      request(server)
+        .post('/api/v1/weather/forecast')
+        .send({
+          name: 'Valencia'
+        })
+        .end((err, res) => {
+          expect(res.body).to.have.property('data');
+          expect(res.body.data).to.be.an('array');
+          expect(res.body.data.length).to.eql(5);
           expect(res).to.have.status(200);
           done();
         });
