6675fa0864202453a9bb5beda3f77a170a2fc588
andrea <andrea.alvarado@4rsoluciones.com>
2020-06-08 22:57:48 -0300

    feat(config): update dotenv config file

--- src/config/env.js
+++ src/config/env.js
@@ -2,11 +2,18 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const { NODE_ENV, PORT, HOST, API_KEY } = process.env;
+const {
+  NODE_ENV,
+  PORT,
+  HOST,
+  WEATHER_BIT_API_KEY,
+  IPAPI_API_KEY
+} = process.env;
 
 module.exports = {
   NODE_ENV: NODE_ENV || 'dev',
   HOST: HOST || '0.0.0.0',
   PORT: PORT || 8000,
-  API_KEY: API_KEY || ''
+  WB_KEY: WEATHER_BIT_API_KEY || '',
+  IP_KEY: IPAPI_API_KEY || ''
 };
