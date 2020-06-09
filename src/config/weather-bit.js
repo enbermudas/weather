50fef1736a572c0884dba632b7f7f196e10cf91f
Enrique Bermúdez <enbermudez96@gmail.com>
2020-06-09 14:29:39 -0300

    refactor(config): redo weather-bit class to reuse code

--- src/config/weather-bit.js
+++ src/config/weather-bit.js
@@ -10,39 +10,81 @@ class WeatherBit {
   }
 
   /**
+   * Returns an error object.
+   *
+   * @param {string} message Error message.
+   */
+  error(message = 'Internal server error') {
+    return { message, code: 500 };
+  }
+
+  /**
+   * Returns the data.
+   *
+   * @param {object} data Data to be returned.
+   */
+  succeed(data) {
+    return { ...data, code: 200 };
+  }
+
+  /**
+   * Check if the city name is defined.
+   *
+   * @param {string} name City name.
+   */
+  async checkName(name) {
+    if (!name) {
+      const { data } = await axios.get(this.ipUri, {
+        params: { access_key: this.ipKey }
+      });
+
+      return { lon: data.longitude, lat: data.latitude };
+    } else {
+      return { city: name };
+    }
+  }
+
+  /**
    * Search for the current weather by city name.
    *
    * @param {string} city City name.
    * @returns {object} the current weather information.
    */
   async current(name) {
-    const params = {
-      key: this.wbKey
-    };
-
     try {
-      if (!name) {
-        const { data } = await axios.get(this.ipUri, {
-          params: {
-            access_key: this.ipKey
-          }
-        });
+      const params = await this.checkName(name);
 
-        params.lat = data.latitude;
-        params.lon = data.longitude;
-      } else {
-        params.city = name;
-      }
+      const { data } = await axios.get(`${this.wbUri}/current`, {
+        params: { ...params, key: this.wbKey }
+      });
 
-      const { data } = await axios.get(`${this.wbUri}/current`, { params });
+      if (typeof data === 'string') throw new Error('No city has been found.');
 
-      if (typeof data === 'string') {
-        throw new Error('No city has been found.');
-      } else {
-        return { ...data, code: 200 };
-      }
+      return this.succeed(data);
     } catch (e) {
-      return { message: 'Internal server error', code: 500 };
+      return this.error();
+    }
+  }
+
+  /**
+   * Search for the forcast weather by city name.
+   *
+   * @param {string} city City name.
+   * @returns {object} the forecast weather information.
+   */
+  async forecast(name) {
+    try {
+      const params = await this.checkName(name);
+
+      const { data } = await axios.get(`${this.wbUri}/forecast/daily`, {
+        params: { ...params, days: 5, key: this.wbKey }
+      });
+
+      if (typeof data === 'string') throw new Error('No city has been found.');
+
+      return this.succeed(data);
+    } catch (e) {
+      return this.error();
     }
   }
 }
